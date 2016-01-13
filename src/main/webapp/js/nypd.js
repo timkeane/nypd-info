ee94110e64194896000400419e05118bd48b0473
timkeane <tkeane@doitt.nyc.gov>
2016-01-13 14:34:37 -0500

    .

--- src/main/webapp/js/nypd.js
+++ src/main/webapp/js/nypd.js
@@ -54,8 +54,8 @@ function zoomToPrecinct(precinctFeature){
 	console.info('Your precinct house: ', houseFeature.getProperties());
 };
 
-function getOrdinal(n) {
-   var s = ["th","st","nd","rd"], v = n % 100;
+function getOrdinal(n){
+   var s = ['th', 'st', 'nd', 'rd'], v = n % 100;
    return n + (s[(v - 20) % 10] || s[v] || s[0]);
 };
 		
