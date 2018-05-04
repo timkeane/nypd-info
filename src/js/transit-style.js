7f18c393fb3c9c6c04012af130832e699b7cb16f
timkeane <tkeane@doitt.nyc.gov>
2018-05-04 16:45:18 -0400

    a mess

--- src/js/transit-style.js
+++ src/js/transit-style.js
@@ -1,112 +1,98 @@
 var STYLE = {
-	stationCache: {},
-	lineCache: {},
 	color: {
-		1: '#ff3433',
-		2: '#ff3433',
-		3: '#ff3433',
+		1: '255,52,51',
+		2: '255,52,51',
+		3: '255,52,51',
 
-		4: '#009d33',
-		5: '#009d33',
-		6: '#009d33',
-		7: '#c801cc',
+		4: '0,157,51',
+		5: '0,157,51',
+		6: '0,157,51',
+		
+		7: '200,1,204',
 
-		A: '#0e689a',
-		C: '#0e689a',
-		E: '#0e689a',
+		A: '14,104,154',
+		C: '14,104,154',
+		E: '14,104,154',
 
-		B: '#fa9705',
-		D: '#fa9705',
-		F: '#fa9705',
+		B: '250,151,5',
+		D: '250,151,5',
+		F: '250,151,5',
 
-		G: '#98cd01',
+		G: '152,205,1',
 
-		J: '#9d6400',
-		M: '#9d6400',
-		Z: '#9d6400',
+		J: '157,100,0',
+		M: '157,100,0',
+		Z: '157,100,0',
 
-		L: '#999999',
-		S: '#999999',
+		L: '153,153,153',
+		S: '153,153,153',
 
-		N: '#ffff0c',
-		R: '#ffff0c',
-		Q: '#ffff0c'
+		N: '255,255,12',
+		R: '255,255,12',
+		Q: '255,255,12'
 	},
+	labeled: {},
 	zoom: function(resolution){
 		return nyc.ol.TILE_GRID.getZForResolution(resolution) - 8;
 	},
 	line: function(feature, resolution){
-		var zoom = STYLE.zoom(resolution),
-			line = feature.get('RT_SYMBOL'),
-			width = [1, 1, 1, 1, 1, 2, 4, 6, 7, 8, 9, 10, 11, 12][zoom];
-		STYLE.lineCache[zoom] = STYLE.lineCache[zoom] || {};
-		if (!STYLE.lineCache[zoom][line]){
-			STYLE.lineCache[zoom][line] = new ol.style.Style({
-				stroke: new ol.style.Stroke({
-					color: STYLE.color[line],
-					width: width
-				})
-			});
-		}
-		return STYLE.lineCache[zoom][line];
-	},
-	selection: function(feature, resolution){
-		var zoom = STYLE.zoom(resolution),
-			radius = [6, 6, 10, 10, 10, 14, 18, 22, 26, 34, 50][zoom - 4],
-			text;
-		var circle = new ol.style.Style({
-			image: new ol.style.Circle({
-				radius: radius,
-				fill: new ol.style.Fill({
-					color: 'rgba(255,255,0,0.8)'
-				})
+		var zoom = STYLE.zoom(resolution);
+		var line = feature.get('RT_SYMBOL');
+		var color = STYLE.color[line];
+		color = feature.active ? ('rgb(' + color + ')') : ('rgba(' + color + ',.4)');
+		width = [1, 1, 1, 1, 1, 2, 4, 6, 7, 8, 9, 10, 11, 12][zoom];
+		return new ol.style.Style({
+			stroke: new ol.style.Stroke({
+				color: color,
+				width: feature.active ? (width * 1.5) : width
 			})
 		});
-		if (feature.selected || zoom > 5){
-			var label = feature.get('NAME');
-			var offsetY = 0;
-			if (label.indexOf('/') > -1){
-				label = label.replace(/\//g, '-\n');
-				offsetY = 10;
-			}
-			text = new ol.style.Style({
+	},
+	text: function(feature, resolution){
+		var zoom = STYLE.zoom(resolution);
+		var selected = feature.selected;
+		var label = feature.label;
+		STYLE.labeled[zoom] = {};
+		if (zoom > 5 && !STYLE.labeled[zoom][label]){
+			STYLE.labeled[zoom][label] = true;
+			var offsetY = label.indexOf('\n') > -1 ? 4 : 0;
+			var font = 'px "Helvetica Neue", Helvetica, Arial, sans-serif' 
+			font = feature.active ? ('bold ' + (zoom * 1.5) + font) : ('bold ' + zoom + font);
+			return new ol.style.Style({
 				text: new ol.style.Text({
 					text: label,
-					font: 'bold 16px "Helvetica Neue", Helvetica, Arial, sans-serif',
+					font: font,
 					textAlign: 'left',
 					textBaseline: 'top',
-					offsetX: 10,
+					offsetX: 9,
 					offsetY: offsetY,
 					fill: new ol.style.Fill({
-						color: '#085095'
+						color: feature.active ? '#000' : 'rgba(0,0,0,.7)'
 					}),
 					stroke: new ol.style.Stroke({
-						width: 5,
-						color: feature.selected ? '#ffff00' : '#fff'
+						width: 2,
+						color: '#fff'
 					})
 				})
-			});
-			return [text, circle, STYLE.station(feature, resolution)];
+			})
 		}
-		return [circle, STYLE.station(feature, resolution)];
 	},
 	station: function(feature, resolution){
 		var zoom = STYLE.zoom(resolution),
 			radius = [2, 2, 4, 4, 4, 6, 8, 10, 12, 16, 24][zoom - 4];
-		if (!STYLE.stationCache[zoom]){
-			STYLE.stationCache[zoom] = new ol.style.Style({
-				image: new ol.style.Circle({
-					radius: radius,
-					stroke: new ol.style.Stroke({
-						color: '#000',
-						width: radius > 2 ? 2 : 1
-					}),
-					fill: new ol.style.Fill({
-						color: 'rgba(255,255,255,0.9)'
-					})
+		var marker = new ol.style.Style({
+			image: new ol.style.Circle({
+				radius: feature.active ? (radius * 1.5) : radius,
+				stroke: new ol.style.Stroke({
+					color: feature.active ? '#000' : 'rgba(0,0,0,.5)',
+					width: radius > 2 ? 2 : 1
+				}),
+				fill: new ol.style.Fill({
+					color: feature.active ? 'rgba(255,255,255,0.9)' : 'rgba(255,255,255,0.4)'					
 				})
-			});
-		}
-		return STYLE.stationCache[zoom];
+			})
+		});
+		var text = STYLE.text(feature, resolution);
+		return text ? [marker, text] : marker;
 	}
 };
--- src/js/transit.js
+++ src/js/transit.js
@@ -1,21 +1,32 @@
 var GEOCLIENT_URL = 'https://maps.nyc.gov/geoclient/v1/search.json?app_key=E2857975AA57366BC&app_id=nyc-gov-nypd';
 
-var map, controls, stationSource, stationLayer, lineSource, lineLayer, selectionSource, selectionLayer, bySector;
+var wtf;
+var wtf2;
+
+var map;
+var controls;
+var stationSource; 
+var stationLayer;
+var lineSource; 
+var lineLayer;
+var selection; 
+var activeStations;
 
 var qstr = document.location.search;
 if (qstr){
 	var args = qstr.substring(1).split('&');
-	bySector = {};
+	selection = {};
 	$.each(args, function(){
 		var pair = this.split('=');
 		if (pair.length == 2){
-			bySector[pair[0]] = pair[1];
+			selection[pair[0]] = pair[1];
 		}
 	});
 	var interval = setInterval(function(){
-		if (stationSource && stationSource.getFeatures().length) {
+		if (stationSource && stationSource.featuresloaded && lineSource.getFeatures().length) {
 			clearInterval(interval);
-			zoomToSector(getStations(bySector));
+			getActiveStations();
+			zoomToStations();
 			controls.setFeatures({
 				featureTypeName: 'subway',
 				features: stationSource.getFeatures(),
@@ -26,45 +37,74 @@ if (qstr){
 	}, 200);
 };
 
-function getStations(bySector){
-	var station = bySector.station ? stationSource.getFeatureById(bySector.station) : '';
-	var sector = bySector.sector || (station ? station.get('SECTOR') : '');
-	var district = bySector.district || (station ? station.get('DISTRICT') : '');
-	var features = [];
-	var extent = ol.extent.createEmpty();
-	$.each(stationSource.getFeatures(), function(){
-		var props = this.getProperties()
-		if (props.DISTRICT == district && (!sector || props.SECTOR == sector)){
-			features.push(this);
-			extent = ol.extent.extend(extent, this.getGeometry().getExtent())
-		}
-	});
-	if (station){
-		station.selected = true;
-		if (!features.length){
-			features.push(station);
-		}
-	}
-	return {features: features, extent: extent}
-};
-
-function zoomToSector(stations){
-	if (stations.features.length){
+function zoomToStations(){
+	var features = activeStations.features;
+	if (features.length){
 		var view = map.getView();
-		selectionSource.clear();
-		selectionSource.addFeatures(stations.features);
-		if (stations.features.length > 1){
-			view.fit(stations.extent, {size: map.getSize(), duration: 500});
+		if (features.length > 1){
+			view.fit(activeStations.extent, {size: map.getSize(), duration: 500});
 		}else{
 			view.animate({
-				center: stations.features[0].getGeometry().getCoordinates(),
+				center: features[0].getGeometry().getCoordinates(),
 				zoom: 16
 			});
 		}
 	}
 };
 
+function getActiveStations(){
+	activeStations = {
+		extent: ol.extent.createEmpty(),
+		features: []
+	};
+	$.each(stationSource.getFeatures(), function(){
+		var districtMatch = this.get('DISTRICT') == selection.district;
+		var sectorMatch = this.get('SECTOR') == selection.sector || !selection.sector;
+		this.selected = this.getId() == selection.station;
+		if (districtMatch && sectorMatch){
+			this.active = true;
+			activeStations.features.push(this);
+			activeStations.extent = ol.extent.extend(
+				activeStations.extent, this.getGeometry().getExtent());
+		}
+	})
+	getActiveLines();
+};
+
+function getActiveLines(){
+	$.each(lineSource.getFeatures(), function(){
+		this.active = false;
+	});
+	$.each(activeStations.features, function(){
+		var station = this.getGeometry().getCoordinates();
+		var stationExt = [station[0] - 500, station[0] + 500, station[1] + 500];
+		$.each(lineSource.getFeatures(), function(){
+			if (!this.active){
+				this.active = this.getGeometry().intersectsExtent(stationExt);
+				console.warn(this.active);
+			}
+		});
+	});
+};
+
 var stationDecorator = {
+	extendFeature: function(){
+		var wrapped = '';
+		var label = this.get('NAME').replace('/\//', ' ');
+		if (label.length > 12) {
+			label = label.replace(' /', '|');
+			$.each(label.split(' '), function(_, word){
+				var lines = wrapped.split('\n')
+				if (lines.length && (lines[lines.length - 1] + word).length > 12) {
+					wrapped += ('\n' + word + ' ');
+				}else{
+					wrapped += (word + ' ');
+				}
+			});
+			wrapped = wrapped.replace('|', '/');
+		}
+		this.label = wrapped ? wrapped.trim() : label;
+	},
 	html: function(){
 		var props = this.getProperties()
 		var html = $('<div></div>');
@@ -93,12 +133,13 @@ var stationDecorator = {
 $(document).ready(function(){
 
 	map = new nyc.ol.Basemap({target: $('#map').get(0)});
+	map.labels.base.setOpacity(.5);
 
 	controls = new nyc.ol.control.ZoomSearch(map);
 
 	lineSource = new ol.source.Vector({
-		url: 'subway-line.json',
-		format: new ol.format.TopoJSON
+		url: 'subway_line.json',
+		format: new ol.format.GeoJSON()
 	});
 	lineLayer = new ol.layer.Vector({source: lineSource, style: STYLE.line});
 	map.addLayer(lineLayer);
@@ -116,10 +157,6 @@ $(document).ready(function(){
 	);
 	stationLayer = new ol.layer.Vector({source: stationSource, style: STYLE.station});
 	map.addLayer(stationLayer);
-
-	selectionSource = new ol.source.Vector({});
-	selectionLayer = new ol.layer.Vector({source: selectionSource, style: STYLE.selection});
-	map.addLayer(selectionLayer);
 
 	new nyc.ol.FeatureTip(map, [{
 		layer: stationLayer,
@@ -170,4 +207,8 @@ $(document).ready(function(){
 		}
 	});
 
+
+	wtf = new ol.source.Vector();
+	wtf2 = new ol.layer.Vector({source: wtf});
+	map.addLayer(wtf2);
 });
